b8132a302d94147f2e33213b64f200863d0b7424
Alberto Garcia <alb.garciam@gmail.com>
2020-02-19 20:31:56 +0100

    initial commit

---
+++ Common/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Alberto García-Muñoz on 19/02/2020.
+//  Copyright © 2020 SoundApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Colors
+
+This is a sample project to show how to have multiple targets which each one has its own customized XCASSETS file.
